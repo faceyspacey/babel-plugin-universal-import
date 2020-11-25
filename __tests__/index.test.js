c05ccbaaeca96bbb29fec8767f27be75271fb0c0
Guillaume <gui.lebas@gmail.com>
2020-11-25 15:27:42 +0100

    test: reorder test cases
    Reorder test cases to prepare adding new cases aimed at fixing #78.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -23,31 +23,6 @@ pluginTester({
   tests: {
     'import from static path': `
       import('./Component')`,
-    'import from static path relative to grand parent folder': `
-      import('../../Component')`,
-    'import from static path with file extension': `
-      import('./Component.js')`,
-    'import from static template string path': `
-      import(\`./Component\`)`,
-    'import from static template string path relative to grand parent folder': `
-      import(\`../../Component\`)`,
-    'import from static path returned by function assigned to object property': `
-      const obj = { component: () => import('../components/Component') };
-      () => obj.component()`,
-    'import from static path relative to parent sub folder': `
-      import(\`../components/Component\`)`,
-    'import from dynamic template string path relative to sub folder': `
-      import(\`./components/\${page}\`)`,
-    'import from dynamic template string path relative to deep sub folder': `
-      import(\`./components/\${page}/nested/Component\`)`,
-    'import from dynamic template string path relative to deep sub folder (two tags)': `
-      import(\`./components/\${page}/nested/\${another}Component\`)`,
-    'import from dynamic template string path': `
-      import(\`./\${page}\`)`,
-    'import from dynamic template string path relative to grand parent folder': `
-      import(\`../../components/\${page}\`)`,
-    'import returned by async function that should return a thennable without calling .then': `
-      async ({ page }) => await import(\`../components/\${page}\`)`,
     'import with babelServer: true': {
       code: `import('./Component')`,
       pluginOptions: { babelServer: true }
@@ -56,12 +31,37 @@ pluginTester({
       code: `import('./Component')`,
       pluginOptions: { disableWarnings: true }
     },
-    'import with custom webpackChunkName': `
-      import(/* webpackChunkName: 'Bar' */'./Component')`,
+    'import from static path with file extension': `
+      import('./Component.js')`,
+    'import from static path relative to grand parent folder': `
+      import('../../Component')`,
+    'import from static path relative to parent sub folder': `
+      import(\`../components/Component\`)`,
+    'import from static template string path': `
+      import(\`./Component\`)`,
+    'import from static template string path relative to grand parent folder': `
+      import(\`../../Component\`)`,
+    'import from dynamic template string path': `
+      import(\`./\${page}\`)`,
+    'import from dynamic template string path relative to sub folder': `
+      import(\`./components/\${page}\`)`,
+    'import from dynamic template string path relative to deep sub folder': `
+      import(\`./components/\${page}/nested/Component\`)`,
+    'import from dynamic template string path relative to deep sub folder (two tags)': `
+      import(\`./components/\${page}/nested/\${another}Component\`)`,
+    'import from dynamic template string path relative to grand parent folder': `
+      import(\`../../components/\${page}\`)`,
+    'import from static path returned by function assigned to object property': `
+      const obj = { component: () => import('../components/Component') };
+      () => obj.component()`,
+    'import returned by async function that should return a thennable without calling .then': `
+      async ({ page }) => await import(\`../components/\${page}\`)`,
     'multiple imports': `
       import('one')
       import('two')
       import('three')`,
+    'import with custom webpackChunkName': `
+      import(/* webpackChunkName: 'Bar' */'./Component')`,
     'import with custom webpackChunkName + webpackmode': `
       import(/* webpackChunkName: 'Bar'*//* webpackMode: 'Lazy' */'./Component')`,
     'import with custom webpackChunkName + webpackmode + webpackInclude': `
@@ -85,42 +85,6 @@ pluginTester({
         /* webpackExclude: /(?!*test.js)$/ */
         /* webpackIgnore: true */
         './Component')`,
-    'import with custom webpackChunkName + strips out unknown things': `
-      import(
-        /* webpackFakeProperty1: 'Lazy' */
-        /* webpackChunkName: 'Bar'*/
-        /* webpackFakeProperty3123: /foobar/ */
-        './Component')`,
-    'import with custom webpackmode + webpackInclude + webpackExclude + webpackIgnore': `
-      import(
-        /* webpackMode: 'Lazy' */
-        /* webpackInclude: /*.js$/ */
-        /* webpackExclude: /(?!*test.js)$/ */
-        /* webpackIgnore: true */
-        './Component')`,
-    'import with custom webpackmode + webpackInclude + webpackExclude': `
-      import(
-        /* webpackMode: 'Lazy' */
-        /* webpackInclude: /*.js$/ */
-        /* webpackExclude: /(?!*test.js)$/ */
-        './Component')`,
-    'import with custom webpackmode + webpackInclude': `
-      import(
-        /* webpackMode: 'Lazy' */
-        /* webpackInclude: /*.js$/ */
-        './Component')`,
-    'import with custom webpackmode': `
-      import(
-        /* webpackMode: 'Lazy' */
-        './Component')`,
-    'import with custom webpackPreload': `
-      import(
-        /* webpackPreload: true */
-        './Component')`,
-    'import with custom webpackPrefetch': `
-      import(
-        /* webpackPrefetch: true */
-        './Component')`,
     'import with custom webpackChunkName + webpackPreload': `
       import(
         /* webpackChunkName: 'Bar'*/
@@ -129,6 +93,42 @@ pluginTester({
     'import with custom webpackChunkName + webpackPrefetch': `
       import(
         /* webpackChunkName: 'Bar'*/
+        /* webpackPrefetch: true */
+        './Component')`,
+    'import with custom webpackChunkName + strips out unknown things': `
+      import(
+        /* webpackFakeProperty1: 'Lazy' */
+        /* webpackChunkName: 'Bar'*/
+        /* webpackFakeProperty3123: /foobar/ */
+        './Component')`,
+    'import with custom webpackmode': `
+      import(
+        /* webpackMode: 'Lazy' */
+        './Component')`,
+    'import with custom webpackmode + webpackInclude': `
+      import(
+        /* webpackMode: 'Lazy' */
+        /* webpackInclude: /*.js$/ */
+        './Component')`,
+    'import with custom webpackmode + webpackInclude + webpackExclude': `
+      import(
+        /* webpackMode: 'Lazy' */
+        /* webpackInclude: /*.js$/ */
+        /* webpackExclude: /(?!*test.js)$/ */
+        './Component')`,
+    'import with custom webpackmode + webpackInclude + webpackExclude + webpackIgnore': `
+      import(
+        /* webpackMode: 'Lazy' */
+        /* webpackInclude: /*.js$/ */
+        /* webpackExclude: /(?!*test.js)$/ */
+        /* webpackIgnore: true */
+        './Component')`,
+    'import with custom webpackPreload': `
+      import(
+        /* webpackPreload: true */
+        './Component')`,
+    'import with custom webpackPrefetch': `
+      import(
         /* webpackPrefetch: true */
         './Component')`
   }
